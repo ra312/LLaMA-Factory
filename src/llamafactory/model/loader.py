50bd56c7349a8c4e4fc9431d019a56cd5db5b289
Rauan Akylzhanov <akylzhanov.r@gmail.com>
2025-04-04 15:35:55 +0000

    allow next token finetune

--- src/llamafactory/model/loader.py
+++ src/llamafactory/model/loader.py
@@ -67,6 +67,40 @@ def _get_init_kwargs(model_args: "ModelArguments") -> dict[str, Any]:
         "token": model_args.hf_hub_token,
     }
 
+def load_gpt_tokenizer(model_args):
+    from transformers import GPT2TokenizerFast
+
+    # Load tokenizer from the specified model
+    
+    
+    init_kwargs = _get_init_kwargs(model_args)
+    try:
+        tokenizer = GPT2TokenizerFast.from_pretrained("Xenova/gpt-4")
+    except ValueError:  # try the fast one
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_args.model_name_or_path,
+            use_fast=True,
+            padding_side="right",
+            **init_kwargs,
+        )
+    except Exception as e:
+        raise OSError("Failed to load tokenizer.") from e
+
+    patch_tokenizer(tokenizer, model_args)
+    try:
+        processor = AutoProcessor.from_pretrained(model_args.model_name_or_path, **init_kwargs)
+        patch_processor(processor, tokenizer, model_args)
+    except Exception as e:
+        logger.debug(f"Processor was not found: {e}.")
+        processor = None
+
+    # Avoid load tokenizer, see:
+    # https://github.com/huggingface/transformers/blob/v4.40.0/src/transformers/models/auto/processing_auto.py#L324
+    if processor is not None and "Processor" not in processor.__class__.__name__:
+        processor = None
+
+    return {"tokenizer": tokenizer, "processor": processor}
+
 
 def load_tokenizer(model_args: "ModelArguments") -> "TokenizerModule":
     r"""Load pretrained tokenizer and optionally loads processor.
--- src/llamafactory/train/sft/workflow.py
+++ src/llamafactory/train/sft/workflow.py
@@ -27,6 +27,11 @@ from ..trainer_utils import create_modelcard_and_push
 from .metric import ComputeAccuracy, ComputeSimilarity, eval_logit_processor
 from .trainer import CustomSeq2SeqTrainer
 
+from llamafactory.data.data_utils import DatasetModule
+from transformers import GPT2TokenizerFast
+from datasets import load_dataset
+from multiprocessing import cpu_count
+
 
 if TYPE_CHECKING:
     from transformers import Seq2SeqTrainingArguments, TrainerCallback
@@ -131,3 +136,136 @@ def run_sft(
 
     # Create model card
     create_modelcard_and_push(trainer, model_args, data_args, training_args, finetuning_args)
+
+
+def load_gpt4_tokenizer():
+    
+
+    # Load tokenizer from the specified model
+    tokenizer = GPT2TokenizerFast.from_pretrained("Xenova/gpt-4")
+
+    return tokenizer
+
+    
+def run_sft_next_token(
+    tokenizer,
+    model,
+    
+    model_args: "ModelArguments",
+    # data_args: "DataArguments",
+    training_args: "Seq2SeqTrainingArguments",
+    finetuning_args: "FinetuningArguments",
+    generating_args: "GeneratingArguments",
+    callbacks: Optional[list["TrainerCallback"]] = None,
+):
+    
+    tokenizer_module = load_tokenizer(model_args)
+    tokenizer = tokenizer_module["tokenizer"]
+    dataset_module = DatasetModule()
+    
+    num_proc = int(0.8 * cpu_count())
+    dataset = load_dataset("togethercomputer/RedPajama-Data-1T", name="arxiv", split="train", num_proc=num_proc)
+
+    # Split: 90% train, 10% evaluation
+    split_dataset = dataset.train_test_split(test_size=0.1, seed=42)
+
+    train_dataset = split_dataset["train"]
+    eval_dataset = split_dataset["test"]
+    
+    dataset_module = {
+        'train_dataset': train_dataset,
+        'eval_dataset': eval_dataset,
+    }
+    dataset_module['train_dataset'] = train_dataset
+    dataset_module['eval_dataset'] = eval_dataset
+    
+    
+    # template = get_template_and_fix_tokenizer(tokenizer, data_args)
+    # dataset_module = get_dataset(template, model_args, data_args, training_args, stage="sft", **tokenizer_module)
+    model = load_model(tokenizer, model_args, finetuning_args, training_args.do_train)
+
+    if getattr(model, "is_quantized", False) and not training_args.do_train:
+        setattr(model, "_hf_peft_config_loaded", True)  # hack here: make model compatible with prediction
+
+    # data_collator = SFTDataCollatorWith4DAttentionMask(
+    #     template=template,
+    #     model=model if not training_args.predict_with_generate else None,
+    #     pad_to_multiple_of=8 if training_args.do_train else None,  # for shift short attention
+    #     label_pad_token_id=IGNORE_INDEX if data_args.ignore_pad_token_for_loss else tokenizer.pad_token_id,
+    #     block_diag_attn=model_args.block_diag_attn,
+    #     attn_implementation=getattr(model.config, "_attn_implementation", None),
+    #     compute_dtype=model_args.compute_dtype,
+    #     **tokenizer_module,
+    # )
+
+    # Override the decoding parameters of Seq2SeqTrainer
+    # training_args.generation_max_length = training_args.generation_max_length or data_args.cutoff_len
+    # training_args.generation_num_beams = data_args.eval_num_beams or training_args.generation_num_beams
+    # training_args.remove_unused_columns = False  # important for multimodal dataset
+
+    # Metric utils
+    metric_module = {}
+    if training_args.predict_with_generate:
+        metric_module["compute_metrics"] = ComputeSimilarity(tokenizer=tokenizer)
+    elif finetuning_args.compute_accuracy:
+        metric_module["compute_metrics"] = ComputeAccuracy()
+        metric_module["preprocess_logits_for_metrics"] = eval_logit_processor
+
+    # Keyword arguments for `model.generate`
+    gen_kwargs = generating_args.to_dict(obey_generation_config=True)
+    gen_kwargs["eos_token_id"] = [tokenizer.eos_token_id] + tokenizer.additional_special_tokens_ids
+    gen_kwargs["pad_token_id"] = tokenizer.pad_token_id
+    gen_kwargs["logits_processor"] = get_logits_processor()
+    # Load the dataset
+    
+
+    
+    # Initialize our Trainer
+    trainer = CustomSeq2SeqTrainer(
+        model=model,
+        args=training_args,
+        finetuning_args=finetuning_args,
+        data_collator=None,
+        callbacks=callbacks,
+        gen_kwargs=gen_kwargs,
+        **dataset_module,
+        **tokenizer_module,
+        # **metric_module,
+    )
+    # from transformers import Trainer
+    # trainer = Trainer()
+
+    # Training
+    if training_args.do_train:
+        train_result = trainer.train(resume_from_checkpoint=training_args.resume_from_checkpoint)
+        trainer.save_model()
+        # if finetuning_args.include_effective_tokens_per_second:
+        #     train_result.metrics["effective_tokens_per_sec"] = calculate_tps(
+        #         dataset_module["train_dataset"], train_result.metrics, stage="sft"
+        #     )
+
+        trainer.log_metrics("train", train_result.metrics)
+        trainer.save_metrics("train", train_result.metrics)
+        trainer.save_state()
+        if trainer.is_world_process_zero() and finetuning_args.plot_loss:
+            plot_loss(training_args.output_dir, keys=["loss", "eval_loss", "eval_accuracy"])
+
+    if training_args.predict_with_generate:
+        tokenizer.padding_side = "left"  # use left-padding in generation
+
+    # Evaluation
+    if training_args.do_eval:
+        metrics = trainer.evaluate(metric_key_prefix="eval", **gen_kwargs)
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+    # Predict
+    # if training_args.do_predict:
+    #     logger.warning_rank0_once("Batch generation can be very slow. Consider using `scripts/vllm_infer.py` instead.")
+    #     predict_results = trainer.predict(dataset_module["eval_dataset"], metric_key_prefix="predict", **gen_kwargs)
+    #     trainer.log_metrics("predict", predict_results.metrics)
+    #     trainer.save_metrics("predict", predict_results.metrics)
+    #     trainer.save_predictions(dataset_module["eval_dataset"], predict_results, generating_args.skip_special_tokens)
+
+    # Create model card
+    # create_modelcard_and_push(trainer, model_args, data_args, training_args, finetuning_args)
